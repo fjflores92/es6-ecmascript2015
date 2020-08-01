12b8ef73a8d3dfecac998d4abcefc5a9ab5a017e
Francisco Flores <fflores.ic.ipn@hotmail.com>
2020-07-31 20:48:20 -0500

    es7 includes y exponencial

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+// Junio 2016
+
+// es7 includes
+
+let numbers = [1,2,3,4,5];
+
+(numbers.includes(3)) ? console.log('Si esta incluido el valor 7') : console.log('No esta incluido el valor 7');
+
+// es7 exponencial
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
